71659175948e28beaf61db7a806dd0cc3fca1200
“Carlino <carlinoo@icloud.com>
2017-11-18 17:20:41 +0000

    routes fixes

--- app/views/book/index.php
+++ app/views/book/index.php
@@ -3,14 +3,16 @@ This is book/index
 <br><br><br>
 <script>
 //
-// send_ajax('POST', '/Vinum/book/json', null, function(data) {
-//   console.log("SUCESS");
-//   console.log(data);
-// }, function(data){
-//   console.log("ERROR");
-//   console.log(data);
-// });
+send_ajax('POST', '/~mac/Vinum/book/json', null, function(data) {
+  console.log("SUCESS");
+  console.log(data);
+}, function(data){
+  console.log("ERROR");
+  console.log(data);
+});
 
 </script>
+
+<?php echo get_json_path; ?>
 
 <br><br><br>
