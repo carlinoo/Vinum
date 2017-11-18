8ca75321ca56956bb6266f1dad19403487c9e8d7
Carlino <carlinoo@icloud.com>
2017-11-18 22:58:15 +0000

    created abstract class Vinum, and constructor

--- app/controllers/book_controller.php
+++ app/controllers/book_controller.php
@@ -4,6 +4,12 @@
 
     function index() {
       self::render();
+
+      $b = new Book(array("son" => "father",
+                          "category_id" => "1"));
+
+      var_dump($b);
+
     }
 
     function json() {
@@ -13,7 +19,7 @@
     function display($id) {
 
       echo "<br><br>";
-      // var_dump($_GET);
+      var_dump($_GET);
 
       echo "<br><br>";
 
--- app/views/book/index.php
+++ app/views/book/index.php
@@ -2,7 +2,7 @@
 <br><br><br>
 <script>
 //
-send_ajax('POST', '/Vinum/book/json', null, function(data) {
+send_ajax('POST', '/get_json', null, function(data) {
   console.log("SUCESS");
   console.log(data);
 }, function(data){
