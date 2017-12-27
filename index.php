3da929e432f05ffe62daf843424ac91aae0d6cc0
carlinoo <carlinoo@icloud.com>
2017-12-27 22:33:10 +0000

    first

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 
   // We require the $_ENV variables and the connection to the database. We also require the initializers functions
   require_once('core/init.php');
+
+  require __DIR__ . '/vendor/autoload.php';
   // require_once('app/views/layout/layout.php');
 
 ?>
