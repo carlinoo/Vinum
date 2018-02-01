3bbd213ca550a2cb699febd2ead525f3b5d9a98a
Carlino <carlinoo@icloud.com>
2018-02-01 15:25:03 +0000

    added details

---
+++ config/variables_copy.php
@@ -0,0 +1,8 @@
+<?php
+  $_ENV['database_name'] = '';
+  $_ENV['database_password'] = '';
+  $_ENV['database_host'] = '';
+  $_ENV['database_username'] = '';
+  $_ENV['root_path'] = 'http://localhost:690/index.php';
+  $_ENV['root_dir'] = 'http://localhost:690/';
+ ?>
